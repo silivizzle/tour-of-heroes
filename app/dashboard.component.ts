83c2f7efe092575e7dfab33b4bc276671a72fa01
silivizzle <tiberius220@gmail.com>
2016-07-29 18:01:17 -0500

    mapped the router to the Dashboard component and populated the detail function

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit }        from '@angular/core';
-import { Hero }                     from './hero';
-import { HeroService }              from './hero.service';
+import { Component, OnInit }    from '@angular/core';
+import { Router }               from '@angular/router'
+import { Hero }                 from './hero';
+import { HeroService }          from './hero.service';
 
 @Component({
     selector: 'my-dashboard',
@@ -10,11 +11,17 @@ export class DashboardComponent implements OnInit {
 
     heroes: Hero[];
 
-    constructor(private heroService: HeroService) { }
+    constructor(
+        private heroService: HeroService,
+        private router: Router
+    ) { }
 
     ngOnInit() {
         this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1,5))
     }
 
-    goToDetail(){}
+    goToDetail(hero: Hero) {
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
+    }
 }
