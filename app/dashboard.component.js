2d54f03f3509028d551b74d04337eeb2fba26e52
tiberius silivestru <tsilivestru@conversantmedia.com>
2016-07-29 16:04:27 -0500

    expanded on the DashBoard component class to add properties and functions

--- app/dashboard.component.js
+++ app/dashboard.component.js
@@ -9,15 +9,22 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var hero_service_1 = require('./hero.service');
 var DashboardComponent = (function () {
-    function DashboardComponent() {
+    function DashboardComponent(heroService) {
+        this.heroService = heroService;
     }
+    DashboardComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes.slice(1, 5); });
+    };
+    DashboardComponent.prototype.goToDetail = function () { };
     DashboardComponent = __decorate([
         core_1.Component({
             selector: 'my-dashboard',
             templateUrl: 'app/dashboard.component.html'
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [hero_service_1.HeroService])
     ], DashboardComponent);
     return DashboardComponent;
 }());
