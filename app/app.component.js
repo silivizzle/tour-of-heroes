e96205ccce864114e2c0cb0d1483891e911ed1ae
silivizzle <tiberius220@gmail.com>
2016-08-03 07:53:58 -0500

    implemented the hero search component into the dashboard

--- app/app.component.js
+++ app/app.component.js
@@ -11,6 +11,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
 var hero_service_1 = require('./hero.service');
+require('./rxjs-extensions');
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Heroes';
