d2df8dfe5bb36b4a8f99ca6666e13c570fabe5d6
silivizzle <tiberius220@gmail.com>
2016-08-02 19:21:27 -0500

    added the in-memory web api configuration

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,9 @@
+// Imports for loading & configuring the in-memory web api
+import { XHRBackend } from '@angular/http';
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }               from './in-memory-data.service';
+
+// Regular bootstrapping procedures after this comment
 import { bootstrap }            from '@angular/platform-browser-dynamic';
 import { AppComponent }         from './app.component';
 import { appRouterProviders }   from './app.routes';
