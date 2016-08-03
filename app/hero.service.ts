19f8603eadcfb69b04ac6bfeac2461f434ca9f2a
silivizzle <tiberius220@gmail.com>
2016-08-03 07:38:37 -0500

    added hero search service

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,7 +5,7 @@ import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
-    private heroesUrl = 'app/heroes';  
+    private heroesUrl = 'app/heroes';
     constructor(private http: Http) { }
     getHeroes() {
         return this.http.get(this.heroesUrl)
@@ -13,16 +13,19 @@ export class HeroService {
                 .then(response => response.json().data as Hero[])
                 .catch(this.handleError);
     }
+
     getHero(id: number) {
         return this.getHeroes()
                 .then(heroes => heroes.find(hero => hero.id === id));
     }
+
     save(hero: Hero): Promise<Hero>  {
         if (hero.id) {
         return this.put(hero);
         }
         return this.post(hero);
     }
+
     delete(hero: Hero) {
         let headers = new Headers();
         headers.append('Content-Type', 'application/json');
@@ -32,6 +35,7 @@ export class HeroService {
                 .toPromise()
                 .catch(this.handleError);
     }
+
     // Add new Hero
     private post(hero: Hero): Promise<Hero> {
         let headers = new Headers({
@@ -42,6 +46,7 @@ export class HeroService {
                 .then(res => res.json().data)
                 .catch(this.handleError);
     }
+
     // Update existing Hero
     private put(hero: Hero) {
         let headers = new Headers();
@@ -53,6 +58,7 @@ export class HeroService {
                 .then(() => hero)
                 .catch(this.handleError);
     }
+
     private handleError(error: any) {
         console.error('An error occurred', error);
         return Promise.reject(error.message || error);
